2928722d6c4fdd114eb4db83190cfca64210de80
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-10-14 23:22:12 -0400

    Update Desing css

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -4,7 +4,7 @@ import { CompletdIcon } from "../Icons/CompletedIcon";
 
 function TodoItem(props) {
   return (
-    <li className="TodoItem">
+    <li className={`TodoItem ${props.completed && "TodoItem--complete"}`}>
       <CompletdIcon completed={props.completed} onComplete={props.onComplete} />
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
